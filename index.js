d21a480bd8462b65cf6a3488b6976451eafe5dd3
Serega Zelinsky <freeman3d@gmail.com>
2024-10-06 15:15:29 -0700

    removed comments

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const arr = [1, 2, 3, 4, 5];
 
-// for (let i = 0; i < arr.length; i++) {
-//   console.log(arr[i]);
-// }
-
 function recursiveIteration(array, index = 0) {
-  // Ваш код тут
   if (index === array.length) return;
   console.log(array[index]);
   index++;
